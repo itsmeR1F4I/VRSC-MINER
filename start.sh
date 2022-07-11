73bec798706ed8e8177817e10868cc49c069af37
marchello0 <108346026+marchello0@users.noreply.github.com>
2022-07-11 16:49:17 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 while [ 1 ];
 do
 chmod +x *
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RMu6YbXcRzim4aSJozwuLu5jzaK57jH5ZU.tst -p x --cpu 4
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u R9ssHyrpwKiG3y7dZAKFz88vvKKQAZwAaM.worer1 -p x --cpu 4
 sleep 9999
 done
